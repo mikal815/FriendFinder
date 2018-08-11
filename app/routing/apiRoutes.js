90a1555ab432fa0a9e7e1b8e06df37cbfbdc2e02
Mike M <mikal815@users.noreply.github.com>
2018-08-10 23:13:51 -0400

    initial setup

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var friendData = require("../data/friends");
+
+
+//Routes
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function(req, res){
+        
+    });
+
+}

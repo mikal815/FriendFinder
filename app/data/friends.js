9b3cec52e4316074eea5aa537204bcb87489059c
Mike M <mikal815@users.noreply.github.com>
2018-08-10 22:26:05 -0400

    put data into friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friendArray =[
+    {
+        "name": "Spicoli",
+        "photo": "https://www.cannabisculture.com/wp-content/uploads/2016/08/film-fast-times-at-ridgemont-high-1982-jeff-spicoli-sean-penn-tshirts-ZsnArN-quote.jpg",
+        "scores": [
+            5,
+            5,
+            3,
+            2,
+            1,
+            4,
+            5,
+            3,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Redman",
+        "photo": "http://archive.massappeal.com/wp-content/uploads/redman-cribs.jpg",
+        "scores": [
+            4,
+            4,
+            5,
+            4,
+            3,
+            2,
+            3,
+            3,
+            3,
+            4
+
+        ]
+    },
+    {
+        "name": "Deniro",
+        "photo": "http://hergunhdfilm.com/uploads/oyuncu/2018/01/robert-de-niro-477.jpg",
+        "scores": [
+            3,
+            4,
+            2,
+            1,
+            5,
+            5,
+            5,
+            3,
+            2,
+            1,
+        ]
+    }
+];
+
+module.exports = friendArray;

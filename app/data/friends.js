133984d580aec1c7c12b7e7a5dde723b0792824a
Mike M <mikal815@users.noreply.github.com>
2018-08-11 21:19:30 -0400

    added compatibility logic and finished jquery on survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,13 +19,13 @@ var friendArray =[
         "name": "Redman",
         "photo": "http://archive.massappeal.com/wp-content/uploads/redman-cribs.jpg",
         "scores": [
-            4,
-            4,
-            5,
-            4,
             3,
             2,
-            3,
+            1,
+            4,
+            2,
+            2,
+            2,
             3,
             3,
             4
@@ -45,6 +45,54 @@ var friendArray =[
             5,
             3,
             2,
+            1,
+        ]
+    },
+    {
+        "name": "Homer",
+        "photo": "https://www.rtor.org/wp-content/uploads/2016/05/homer.jpg",
+        "scores": [
+            5,
+            4,
+            2,
+            2,
+            5,
+            4,
+            3,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        "name": "Slash",
+        "photo": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2010/11/081121_150422_slash1_PA-2-261x320.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            3,
+            4,
+            5,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        "name": "Dave Chappelle",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2018/01/02/dave/02-dave-chappelle-7.w710.h473.jpg",
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            3,
+            4,
+            5,
+            1,
+            3,
             1,
         ]
     }
